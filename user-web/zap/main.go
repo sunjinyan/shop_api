8613d00aa3cd69d2818cdd2d7a7891671154b85d
sunjinyan <13683234298@163.com>
2022-10-25 10:24:25 +0800

    “
    ”

---
+++ user-web/zap/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//cfg := zap.NewProductionConfig()
+	////cfg.OutputPaths 将日志输出到文件参数
+	//
+	//logger, err := cfg.Build()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//logger.Info("s")
+}
